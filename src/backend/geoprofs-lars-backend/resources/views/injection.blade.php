9d34bbdb7b3c9c9c7ece73a3e6807e976d15b880
odinVos <1197607@student.roc-nijmegen.nl>
2024-11-05 10:20:03 +0100

    made a form

---
+++ src/backend/geoprofs-lars-backend/resources/views/injection.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+
+    </head>
+    <body>
+ <h1>
+    Testen: sql injection
+ </h1>
+ <form action="">
+    <input type="text">
+    <input type="submit">
+ </form>
+
+    </body>
+</html>
