031af65ee3178e0120a02c33869616186cf7a483
odinVos <1197607@student.roc-nijmegen.nl>
2024-11-27 09:52:27 +0100

    fixed error

--- src/backend/routes/api.php
+++ src/backend/routes/api.php
@@ -3,5 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\VerlofController;
 
+//Reminder, de URL gaat er zo uitzien: http://localhost:8000/api/admin/verlof/create
 Route::get('/verlof/create', [VerlofController::class, 'create'])->name('verlof.create');
 Route::apiresource('verlof', VerlofController::class);
