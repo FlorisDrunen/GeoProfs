b4aea49ccd11f2971dcca83a7f1ecfb34ed81649
Floris van Drunen <floris.van.drunen@outlook.com>
2025-01-24 10:33:01 +0100

    added routes for the login, register and dashboard screens

--- src/backend/routes/web.php
+++ src/backend/routes/web.php
@@ -1,9 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
+
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+
+Route::get('/register', [AuthController::class, 'showRegister'])->name(name: 'register');
 
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware('auth')->name('dashboard');
