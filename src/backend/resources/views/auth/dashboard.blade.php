0bf1ab66a227699a0900502bb51ac9a7e5b59334
Floris van Drunen <floris.van.drunen@outlook.com>
2025-01-30 14:43:23 +0100

    dashboard css done

--- src/backend/resources/views/auth/dashboard.blade.php
+++ src/backend/resources/views/auth/dashboard.blade.php
@@ -9,11 +9,15 @@
 <body class="dashboard-body">
     <div class="dashboard-header">
         <h1 class="dashboard-header-text">Welcome, {{ Auth::user()->first_name }} {{ Auth::user()->last_name }}</h1>
-        <form class="dashboard-form" method="POST" action="{{ route('logout') }}">
+    </div>
+    <div class="dashboard-form">
+        <form method="POST" action="{{ route('logout') }}">
             @csrf
             <button class="dashboard-button" type="submit">Logout</button>
         </form>
     </div>
+
+
 </body>
 
 </html>
